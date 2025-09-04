ec761f63b04c815c963f6837f5240bbad4bf9a34
snoutiq <snoutiq@gmail.com>
2025-09-04 18:05:03 +0530

    update code login extra feature

--- frontend/src/auth/AuthContext.jsx
+++ frontend/src/auth/AuthContext.jsx
@@ -111,7 +111,9 @@
 //   );
 // };
 
+import React, { createContext, useState, useEffect } from "react";
 
+export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(null);
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -290,7 +290,7 @@ import { useEffect, useState, useContext } from "react";
 import axios from "../axios";
 import toast from "react-hot-toast";
 import { PlusCircleIcon, TrashIcon } from "@heroicons/react/24/outline";
-import { AuthContext } from "../auth/AuthContext";
+import { AuthContext } from '../auth/AuthContext'
 import { useNavigate, useParams } from "react-router-dom";
 
 // Pet Images
