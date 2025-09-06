07da7ffa07b7756ddfcd13de0861df73bd3fba9d
Maayank Malhotra <maayankmalhotra095@gmail.com>
2025-09-06 06:42:16 +0530

    WH FINAL

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -31,7 +31,7 @@ use App\Http\Controllers\Api\ContactRequestController;
 
 Route::get('/chat-rooms/{chat_room_token}/chats', [GeminiChatController::class, 'getRoomChats']);
 Route::get('/test-dd', function () {
-    dd(" 111.0 👋, test route working!  maayank malhotra");
+    dd("webhook is finally working");
 });
 Route::get('/test-cors', function (Request $request) {
     return response()->json([
