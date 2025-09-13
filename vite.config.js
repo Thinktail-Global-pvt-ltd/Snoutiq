7f81bcac81afcaaf52de0e213953b9939f14aba6
snoutiq <snoutiq@gmail.com>
2025-09-13 15:37:17 +0530

    optimize update 3:37

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',        // ✅ Modern browsers को direct ES6 दो
+    target: 'esnext', // ✅ सिर्फ modern JS compile होगा
     modulePreload: { polyfill: false },
     rollupOptions: {
       output: {
