ed82402aa587291dcfbd5eeb067117afc46f1646
snoutiq <snoutiq@gmail.com>
2025-09-15 13:29:21 +0530

    optimize update 1:26

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,19 @@ export default defineConfig({
       output: {
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
-      manualChunks(id) {
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     return 'vendor'; // node_modules ek hi chunk me
+        //   }
+        //   // pages folder ke components ko alag chunks me
+        //   if (id.includes('pages/Home')) {
+        //     return 'home';
+        //   }
+        //   if (id.includes('pages/Login')) {
+        //     return 'login';
+        //   }
+        // }
+        manualChunks(id) {
   if (id.includes('react')) return 'react-vendor';
   if (id.includes('node_modules')) return 'vendor';
   if (id.includes('pages/Home')) return 'home';
