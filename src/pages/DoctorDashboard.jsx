d879b126178551e32c3dd3080902a9169cf7330e
Maayank Malhotra <maayankmalhotra095@gmail.com>
2025-09-19 08:21:07 +0530

    done

--- src/pages/DoctorDashboard.jsx
+++ src/pages/DoctorDashboard.jsx
@@ -9,7 +9,7 @@ const isLocal =
 
 const socket = io(isLocal ? "http://localhost:4000" : "https://snoutiq.com", {
   path: "/socket.io",
-  transports: ["websocket","polling"],
+   transports: ["polling", "websocket"],  
 });
 
 export default function DoctorDashboard({ doctorId = 501 }) {
