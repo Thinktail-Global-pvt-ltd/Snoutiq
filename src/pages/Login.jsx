79d05ed9e4bba0988976b795b2fd00be1e547f18
snoutiq <snoutiq@gmail.com>
2025-10-23 18:28:16 +0530

    update 4:38

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,6 +10,7 @@ import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 // Background image - you can replace this with your actual image
 const loginBackground = "https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80";
 
+
 const Login = () => {
   const [formData, setFormData] = useState({
     email: "",
