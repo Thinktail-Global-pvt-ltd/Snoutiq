0fe5e0834a48ba21ffd39a4acbd1996d710b34ef
maayank <maayank.malhotra@cracode.com>
2026-01-21 15:33:25 +0530

    done

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/js/echo.js',
+                'resources/js/dev/doctor-receiver.js',
             ],
             refresh: true,
         }),
